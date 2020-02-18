05651f6bd8ed35c01b05b0b5fec96495212e8867
Nathan hoarau <nhoarau@mobilis-afm.com>
2020-02-18 10:11:08 +0100

    position controller

--- Controllers/PositionController.php
+++ Controllers/PositionController.php
@@ -5,10 +5,11 @@ include_once 'Models/Coordonate.php';
 class PositionController {
     function post($coordonate)
     {
+        $coordonates = new Coordonate($coordonate["y"], $coordonate["x"]);
         $db = new SQLite3("tp.db");
         $command = $db->prepare("INSERT INTO coordonate (y, x) VALUES (:y,:x);");
-        $command->bindValue(":y", $coordonate->y, SQLITE3_NUM);
-        $command->bindValue(":x", $coordonate->x, SQLITE3_NUM);
+        $command->bindValue(":y", $coordonates->y, SQLITE3_NUM);
+        $command->bindValue(":x", $coordonates->x, SQLITE3_NUM);
         $command->execute();
     }
 }
