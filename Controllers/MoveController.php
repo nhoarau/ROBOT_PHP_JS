8f335ead0f9957e87af2527b3360675e82d1cd1a
Nathan hoarau <nhoarau@mobilis-afm.com>
2020-02-18 10:54:39 +0100

    move controller

--- Controllers/MoveController.php
+++ Controllers/MoveController.php
@@ -1,5 +1,16 @@
 <?php 
 
+include_once 'Models/MoveEnum.php';
+
 class MoveController {
-    
+    function get() {
+        $randomMoves = [];
+
+        for($i = 0; $i <= 4; $i++){
+            array_push($randomMoves, rand(0,3));
+        }
+
+        echo json_encode($randomMoves);
+    }
+
 }
